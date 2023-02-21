719cedb47480a749b2192500046b839e6a81cf2d
santi100a <santyrojasprieto9@gmail.com>
2023-02-20 19:07:59 -0500

    feat: version 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @param array The array to look through. It's highly recommended to sort it before passing it to this function, in order to make it faster.
  * @param target The item to look for.
  * @returns The index of the first ocurrence of `target`, or -1 if it's not present.
- */
+ */ 
 function bisect<T = unknown>(array: T[], target: T): number {
     function __<U>(start: number = 0, end: number = array.length): number {
         if (start > end) return -1;
